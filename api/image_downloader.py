e26d288ec6811d5215a582c071309934aff55c4e
inytar <pietpiet@fastmail.net>
2017-03-25 14:03:37 -0400

    Add test for image downloader

--- api/image_downloader.py
+++ api/image_downloader.py
@@ -22,24 +22,20 @@ class ImageDownloader(Process):
     def __init__(self, csv_file, charset):
         self.csv_file = csv_file
         self.charset = charset
-        super().__init__()
+        super().__init__(daemon=True)
 
     def run(self):
         # Close connections to database, to prevent problems.
         db.connections.close_all()
         for row in csv_reader(self.csv_file, self.charset):
-            print('reading row {}'.format(row))
             for value in row:
                 # Skip already saved files.
                 if Image.objects.filter(original_url__iexact=value):
-                    print('Already saved')
                     continue
                 image_file, image_type = download_image(value)
                 if image_file is None:
-                    print('not an image')
                     continue
                 random_string = get_random_string(RANDOM_FILENAME_LENGTH)
-                print('saving image')
                 # Create database image object and save it.
                 image = Image(original_url=value, type=image_type)
 
@@ -48,8 +44,6 @@ class ImageDownloader(Process):
                     '{}.original.{}'.format(random_string, image_type),
                     image_file, save=True
                 )
-                print('saved image {}'.format(value))
-        print('Done with upload')
 
 
 def download_image(url):
@@ -57,13 +51,14 @@ def download_image(url):
     if not url or not rfc3987.match(url, 'absolute_URI'):
         return None, None
     # Get url.
-    # TODO timeout checks.
-    response = requests.get(url)
+    try:
+        response = requests.get(url)
+    except requests.exceptions.ConnectionError:
+        return None, None
     if response.status_code == 200:
         image = BytesIO(response.content)
         # Check that the file is really an image.
         image_type = imghdr.what(image)
         if image_type:
-            print('got image {}'.format(url))
             return ImageFile(image), image_type
     return None, None
--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,55 @@
+import os
+
+from django.core.files.images import ImageFile
 from django.test import TestCase
 
+import requests
+
+import requests_mock
+
 # Create your tests here.
+
+from api import image_downloader
+
+
+class TestImageDownloader(TestCase):
+
+    image = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                         'test_image.jpeg')
+
+    @requests_mock.Mocker()
+    def test_download_image(self, m):
+        # Test cases with url an expected result.
+        test_cases = [
+            ('not_an_url', (object, None)),
+            ('', (object, None)),
+            (None, (object, None)),
+            ('http://example.com/404', (object, None)),
+            ('http://example.com/no_image', (object, None)),
+            ('http://example.com/time_out', (object, None)),
+            ('/not_absolute', (object, None)),
+            ('example.com/image', (object, None)),  # not absolute.
+            ('/example.com/image', (object, None)),  # not absolute.
+            ('http://example.com/image', (ImageFile, 'jpeg')),
+            ('http://example.com/redirect', (ImageFile, 'jpeg')),
+        ]
+
+        # Register mock urls
+        m.get('http://example.com/404', status_code=404)
+        m.get('http://example.com/no_image', text='Not an image')
+        m.get('http://example.com/time_out',
+              exc=requests.exceptions.ConnectTimeout)
+        with open(self.image, 'rb') as f:
+            m.get('http://example.com/image', content=f.read())
+        m.get('http://example.com/redirect', status_code=301,
+              headers={'location': 'http://example.com/image'})
+
+        for url, expected in test_cases:
+            print(url, expected)
+            result = image_downloader.download_image(url)
+            self.assertEqual(result[1], expected[1])
+            self.assertTrue(isinstance(result[0], expected[0]))
+
+
+class TestCsvView(TestCase):
+    pass
